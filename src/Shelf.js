df4364ee0dbb26dc074ced6a886cac3a5b805d8f
Roger Graves <rogergraves@gmail.com>
2018-08-03 17:31:57 -0700

    Hide shelf if there are no books in it

--- src/Shelf.js
+++ src/Shelf.js
@@ -3,21 +3,25 @@ import Book from './Book'
 
 class Shelf extends Component {
   render() {
+    const books = this.props.books.filter((book) => {
+      return book.shelf === this.props.filter_category
+    });
+
+    console.log("For shelf "+this.props.filter_category+" books are: ", books);
+
     return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{this.props.title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {this.props.books.filter((book) => {
-              return book.shelf === this.props.filter_category
-            }).map((book) => (
-              <li key={book.id}><Book info={book} onChangeShelf={this.props.onChangeShelf} /></li>
-            ))}
-          </ol>
+      books.length > 0 && (
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{this.props.title}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {books.map((book) => (
+                <li key={book.id}><Book info={book} onChangeShelf={this.props.onChangeShelf} /></li>
+              ))}
+            </ol>
+          </div>
         </div>
-      </div>
-
-
+      )
     )
   }
 }
